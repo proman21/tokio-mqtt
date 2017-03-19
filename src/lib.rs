6bc13c334b4722cd2b20c81abd0b6d812dfaac8a
Adam Jacobus <adam.m.jacobus@gmail.com>
2017-03-19 17:54:17 +1000

    Add dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_io;
+extern crate tokio_tls;
+
+#[macro_use] extern crate bitflags;
+#[macro_use] extern crate error_chain;
+#[macro_use] extern crate nom;
+#[macro_use] extern crate enum_primitive;
+
 #[cfg(test)]
 mod tests {
     #[test]
