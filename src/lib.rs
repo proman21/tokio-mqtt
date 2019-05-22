a88bf6a0931037cf5403fee9d3a7cdf111db2545
Adam Jacobus <adam.m.jacobus@gmail.com>
2019-05-22 14:25:47 +1000

    Move protocl parsing to separate crate.

--- src/lib.rs
+++ src/lib.rs
@@ -2,44 +2,22 @@
 #![allow(unused_variables)]
 #![recursion_limit="128"]
 
-#[macro_use]
-extern crate futures;
+#[macro_use] extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
 extern crate tokio_tls;
-
-#[macro_use]
-extern crate bitflags;
-#[macro_use]
-extern crate error_chain;
-#[macro_use]
-extern crate nom;
-#[cfg(test)]
-#[macro_use]
-extern crate nom_test_helpers;
-#[macro_use]
-extern crate enum_primitive;
+#[macro_use] extern crate error_chain;
 extern crate bytes;
 extern crate touch;
-extern crate linked_hash_map;
-extern crate regex;
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
+#[macro_use] extern crate lazy_static;
 extern crate bincode;
-#[macro_use]
-extern crate slog;
+#[macro_use] extern crate slog;
 extern crate slog_stdlog;
-#[macro_use]
-extern crate derive_builder;
-
+#[macro_use] extern crate derive_builder;
+extern crate mqtt3_proto
 
 mod errors;
 mod types;
-mod proto;
 mod client;
 mod persistence;
 mod backend;
-
